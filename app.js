75eb6ee93bb8e6b5d314fcca0547cf65aa00cfb3
Joseph Barsness <joseph.barsness@gmail.com>
2017-01-16 11:38:18 -0600

    server spun up and mongodb connection created.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+// var passport = require('./strategies/userStrategy');
+
+//require routers
+// var indexRouter = require('./routes/index');
+// var registerRouter = require('./routes/register');
+// var homeRouter = require('./routes/home');
+// var testRouter = require('./routes/test');
+
+var app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// server port set and listen
+var serverPort = process.env.port || 3000;
+app.set('port', serverPort);
+
+var server = app.listen(serverPort, function() {
+  console.log('up and listening on', server.address().port);
+});
+
+// connect to the mongodb
+var mongoURI = "mongodb://localhost:27017/soundboard";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function (err) {
+    console.log('mongodb connection error:', err);
+});
+
+MongoDB.once('open', function () {
+  console.log('mongodb connection open!');
+});
